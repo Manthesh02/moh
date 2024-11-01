989eb33fe10c6e4cdff368a1f70263bbae856bb5
Manthesh02 <64277546+Manthesh02@users.noreply.github.com>
2024-11-01 17:01:22 +0530

    Update sharedParams.groovy

--- vars/sharedParams.groovy
+++ vars/sharedParams.groovy
@@ -7,12 +7,10 @@ def call() {
             choiceType: 'CHECKBOX',
             script: [
                 classpath: [],
-                fallbackScript: [
-                    classpath: [],
-                    script: 'return ["default-site:0.0.0.0"]'
-                ],
-                script: 'return ["MHHTP:10.5.43.89", "LGHJP:10.5.43.93"]'
+                script: 'return ["MHHTP:10.5.43.89","LGHJP:10.5.43.93"]'
             ]
-        )
+        ),
+        // Add more parameters as needed
+        string(name: 'VERSION', defaultValue: '1.0.0', description: 'Specify the Version to deploy')
     ]
 }
