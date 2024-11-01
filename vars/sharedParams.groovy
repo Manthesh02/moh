b31ac56f02ccd5e0b6554a5b7cbef56d60c7a4e2
Manthesh02 <64277546+Manthesh02@users.noreply.github.com>
2024-11-01 17:18:50 +0530

    Update sharedParams.groovy

--- vars/sharedParams.groovy
+++ vars/sharedParams.groovy
@@ -1,19 +1,14 @@
 def call() {
     return [
-        activeChoice(
-            name: 'SITE',
-            type: 'PT_CHECKBOX',
-            description: 'Select the Site (namespace:IP)',
-            choiceType: 'CHECKBOX',
-            script: [
-                classpath: [],
-                fallbackScript: [
-                    classpath: [],
-                    script: 'return ["Default Site"]'
-                ],
-                script: 'return ["MHHTP:10.5.43.89", "LGHJP:10.5.43.93"]' // Add your actual sites here
-            ]
-        ),
-        string(name: 'VERSION', defaultValue: '1.0.0', description: 'Specify the Version to deploy')
+        activeChoice(name: 'SITE', 
+                     type: 'PT_CHECKBOX', 
+                     description: 'Select the Site (namespace:IP)', 
+                     choiceType: 'CHECKBOX', 
+                     script: [
+                         "return ['Site1', 'Site2', 'Site3']"
+                     ]),
+        string(name: 'VERSION', 
+               defaultValue: '1.0.0', 
+               description: 'Specify the Version to deploy')
     ]
 }
