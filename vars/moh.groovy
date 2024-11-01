2a986c373b0165ee1745c3ef9472c46f1e4b070c
Manthesh02 <64277546+Manthesh02@users.noreply.github.com>
2024-11-01 17:33:31 +0530

    Update and rename sharedParams.groovy to moh.groovy

--- vars/moh.groovy
+++ vars/moh.groovy
@@ -5,10 +5,7 @@ def call() {
                      description: 'Select the Site (namespace:IP)', 
                      choiceType: 'CHECKBOX', 
                      script: [
-                         "return ['Site1', 'Site2', 'Site3']"
-                     ]),
-        string(name: 'VERSION', 
-               defaultValue: '1.0.0', 
-               description: 'Specify the Version to deploy')
+                         """return ['Site1', 'Site2', 'Site3']"""
+                     ])
     ]
 }
