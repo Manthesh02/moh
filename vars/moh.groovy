1f52582579277c49d7e0fb8463d2097b8b1f7e17
Manthesh02 <64277546+Manthesh02@users.noreply.github.com>
2024-11-01 19:39:20 +0530

    Update moh.groovy

--- vars/moh.groovy
+++ vars/moh.groovy
@@ -2,15 +2,15 @@ def call() {
     return [
         activeChoice(name: 'SITES', 
                      description: 'Select the Sites (multiple selections allowed)', 
-                     script: {
+                     script: ''' 
                          return ["MHHTP:10.5.43.89", "LGHJP:10.5.43.93", "RGHPJ:10.5.43.94"]
-                     }
+                     '''
         ),
         activeChoice(name: 'SERVICES', 
                      description: 'Select the Services (multiple selections allowed)', 
-                     script: {
+                     script: ''' 
                          return ["word-report", "dataset-setup", "scm-integration"]
-                     }
+                     '''
         ),
         string(name: 'VERSION', 
                defaultValue: '1.0.0', 
