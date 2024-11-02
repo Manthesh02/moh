46c67e5dc8a95e47c53909978eeeb221f92658f0
Manthesh02 <64277546+Manthesh02@users.noreply.github.com>
2024-11-02 17:16:48 +0530

    Update moh.groovy

--- vars/moh.groovy
+++ vars/moh.groovy
@@ -1,13 +1,9 @@
 class Moh {
     static List<String> fetchSites() {
-        return ["MHHTP:10.5.43.89", "LGHJP:10.5.43.93"] // Update with actual data
+        return ["MHHTP:10.5.43.89", "LGHJP:10.5.43.93"] // Example values
     }
 
     static List<String> fetchServices() {
-        return ["word-report", "dataset-setup"] // Update with actual data
-    }
-
-    static String fetchVersion() {
-        return "1.0.0" // Default version or fetch dynamically if needed
+        return ["word-report", "dataset-setup"] // Example values
     }
 }
